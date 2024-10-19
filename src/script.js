a3de96d55b9d2dfdf11de4d5f51f918308f05dc8
Tochi <oparajitee4@gmail.com>
2024-10-19 06:33:15 -1200

    loaders

--- src/script.js
+++ src/script.js
@@ -9,6 +9,38 @@ const header = document.getElementById('header');
 
 const contact = document.getElementById('contact')
 
+$(window).on('load', function() {
+
+  const svg = document.getElementById("preloaderSvg");
+  const tl = gsap.timeline();
+  const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+  const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+
+  tl.to(".preloader-heading .load-text , .preloader-heading .cont", {
+      delay: 4,
+      y: -100,
+      opacity: 0,
+  });
+  tl.to(svg, {
+      duration: 0.5,
+      attr: { d: curve },
+      ease: "power2.easeIn",
+  }).to(svg, {
+      duration: 0.5,
+      attr: { d: flat },
+      ease: "power2.easeOut",
+  });
+  tl.to(".preloader", {
+      y: -1500,
+  });
+  tl.to(".preloader", {
+      zIndex: -1,
+      display: "none",
+  });
+
+
+
+});
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('openHam');
@@ -140,6 +172,7 @@ document.getElementById('contactForm').addEventListener('submit', function (even
     alert('Network error. Please try again later.');
   });
 });
+
 
 
 
